9eb7396ba13d1dee190f3494b315c785a99a3d32
KornitskiyBV <KornitskiyBV@gnivc.ru>
2026-05-23 17:16:58 +0700

    fix

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -67,7 +67,6 @@ func TestCopy(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			outPath := filepath.Join(t.TempDir(), "out.txt")
