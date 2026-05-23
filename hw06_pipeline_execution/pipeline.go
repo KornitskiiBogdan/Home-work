aa8f4b952a3878149d13330c1dc826376a70ca2a
KornitskiyBV <KornitskiyBV@gnivc.ru>
2026-05-23 14:16:34 +0700

    Реализовано 6тое домашнее задание

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,65 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if len(stages) == 0 {
+		if done == nil {
+			return in
+		}
+		return doneStage(in, done)
+	}
+	out := in
+	for _, stage := range stages {
+		if done != nil {
+			out = doneStage(out, done)
+		}
+		out = stage(out)
+	}
+	if done != nil {
+		out = doneStage(out, done)
+	}
+
+	return out
+}
+
+func doneStage(in In, done In) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-done:
+				go func() {
+					for range in {
+					}
+				}()
+				return
+			default:
+
+			}
+
+			select {
+			case <-done:
+				go func() {
+					for range in {
+					}
+				}()
+				return
+
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case <-done:
+					go func() {
+						for range in {
+						}
+					}()
+					return
+				case out <- v:
+				}
+			}
+		}
+	}()
+	return out
 }
